b1f908d1fbb1e2eb2d171e1bb5d6f2160eb1034b
Marek Dziewit <marek2901@gmail.com>
2018-03-16 00:32:26 +0100

    Fix test :see_no_evil:

--- src/main/components/index.test.js
+++ src/main/components/index.test.js
@@ -4,6 +4,5 @@ import Main from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
